93a144ae13d19e720bf7f371a8084b1a2b18ab62
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-10 05:45:19 +0200

    Initial commit

---
+++ Metoder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Metoder
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      SayThings();
+      //Cursed(0);
+
+      Console.ReadLine();
+
+    }
+
+    static void Cursed(int t)
+    {
+      Console.WriteLine("CURSED: " + t);
+      Cursed(t+1);
+    }
+
+    static void DescribeLivingRoom()
+    {
+      Console.WriteLine("Vardagsrummet är stort och mörkt.");
+      Console.WriteLine("Vardagsrummet är stort och mörkt.");
+      Console.WriteLine("Vardagsrummet är stort och mörkt.");
+      Console.WriteLine("Vardagsrummet är stort och mörkt.");
+      Console.WriteLine("Vardagsrummet är stort och mörkt.");
+      Console.WriteLine("Vardagsrummet är stort och mörkt.");
+    }
+
+    static void SayThings() {
+      DescribeLivingRoom();
+    }
+
+
+  }
+}
